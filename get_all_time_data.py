a72354fd3c4a87a597e7f2e9902841be9f5efb05
Nsonnes <71513789+Nsonnes@users.noreply.github.com>
2023-04-25 00:31:01 +0200

    Update get_all_time_data.py

--- get_all_time_data.py
+++ get_all_time_data.py
@@ -17,6 +17,8 @@ def get_hour():
     end_date = now.strftime("%d")
     end_hour = now.strftime("%H")
     end_month = now.strftime("%m")
+    if int(end_hour) == 0:
+        return [end_hour, end_date, end_month]
     if int(end_hour) < 10:
         end_hour = str(f"0{end_hour}")
         return [end_hour, end_date, end_month]
@@ -28,8 +30,8 @@ def get_data():
     url = f"https://kbh-proxy.septima.dk/api/measurements?stations=2&meanValueTypes=24H&start=2018-01-01T08%3A00%3A00Z&end=2023-{get_hour()[2]}-{get_hour()[1]}T{get_hour()[0]}%3A00%3A00Z"
     req = requests.get(url)
     package_dict = json.loads(req.content)
-    df = pd.json_normalize(package_dict, record_path=["stations", "measurements"])
-    df.to_csv("test_data.csv")
+    return pd.json_normalize(package_dict, record_path=["stations", "measurements"])
+    
 
 def check_limit_PM2(val):
     if val >= 15:
