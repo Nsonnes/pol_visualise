3527e0ef91b6edf5bc694aeed622c2753273ba08
Nsonnes <71513789+Nsonnes@users.noreply.github.com>
2023-04-30 12:58:28 +0200

    Update get_all_time_data.py

--- get_all_time_data.py
+++ get_all_time_data.py
@@ -7,34 +7,30 @@ import json
 import requests
 import datetime as datetime
 import dash_bootstrap_components as dbc
+import numpy as np
 
-# Incorporate data
 
-
-# Morning hours are formatted as 08... and evening hours are formatted as 8
 def get_hour():
     now = datetime.datetime.now()
     end_date = now.strftime("%d")
     end_hour = now.strftime("%H")
     print(end_hour)
-    #print(end_hour)
     end_month = now.strftime("%m")
     if int(end_hour) == 0:
         return [end_hour, end_date, end_month]
-    #if int(end_hour) < 10:
-        #end_hour = str(f"0{end_hour}")
-       # return [end_hour, end_date, end_month]
+
     else:
         return [end_hour, end_date, end_month]
 
 
-def get_data():
-    url = f"https://kbh-proxy.septima.dk/api/measurements?stations=2&meanValueTypes=24H&start=2018-01-01T08%3A00%3A00Z&end=2023-{get_hour()[2]}-{get_hour()[1]}T{get_hour()[0]}%3A00%3A00Z"
+def get_data(val):
+    url = f"https://kbh-proxy.septima.dk/api/measurements?stations={val}&meanValueTypes=24H&start=2018-01-01T08%3A00%3A00Z&end=2023-{get_hour()[2]}-{get_hour()[1]}T{get_hour()[0]}%3A00%3A00Z"
     print(url)
     req = requests.get(url)
 
     package_dict = json.loads(req.content)
     return pd.json_normalize(package_dict, record_path=["stations", "measurements"])
+
 
 def check_limit_PM2(val):
     if val >= 15:
@@ -59,16 +55,13 @@ def check_limit_PM10(val):
 
 def process_data(df):
     df = df[["PM2_5", "NO2", "PM10", "EndLocal"]]
-    #print(df)
+
     df["EndLocal"] = pd.to_datetime(df["EndLocal"])  # format without hour
     df["year"] = df['EndLocal'].dt.year
-    #df["EndLocal"] = df["EndLocal"].dt.floor('D')
-    df["EndLocal"] = df["EndLocal"].dt.date
-    # print(df[["EndLocal"]])
 
-    #df_pm["month"] = df_pm['EndLocal'].dt.month
-    #df["year"] = df_pm['EndLocal'].dt.year
-    #df["day"] = df['EndLocal'].dt.day
+    df["month"] = df['EndLocal'].dt.month
+    df["EndLocal"] = df["EndLocal"].dt.date
+
     df["ExceededPM2"] = df["PM2_5"].apply(check_limit_PM2)
     df["ExceededNO2"] = df["NO2"].apply(check_limit_NO2)
     df["ExceededPM10"] = df["PM10"].apply(check_limit_PM10)
@@ -76,210 +69,123 @@ def process_data(df):
     return df
 
 
-def pivot_data(df, var):
-
-    return df.pivot_table(values=var, columns='EndLocal', aggfunc="first")
-
-
-df = get_data()
-#(df)
-df = process_data(df)
-
-
-# Initialize the app
-#app = Dash(__name__)
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA],  meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LITERA, "https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap"],  meta_tags=[{'name': 'viewport',
+                                                                                                                                                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'},])
 server = app.server
-
-
-
-# App layout
-
-
-# Add controls to build the interaction
-
-
-
-
-
-
-# Run the app
-
-
-
 
 
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
-            html.H1("Luftforurening i København")
-        ], xs=9, lg=10, xl=8),
+            html.H1([
+                "Luftforurening: Overskridelser af WHO grænseværdier"]),
+            html.H2(id="heading")
+
+        ], xs=9, lg=10, xl=12, class_name="heading"),
     ], justify="center"),
 
     dbc.Row([
         dbc.Col([html.Div(style={"margin-top": '70px'}),
             html.Div([
-    dcc.RangeSlider(marks={2020:"2020", 2021:"2021", 2022:"2022", 2023:"2023"}, step=1, min=2020,max=2023, value=[2020,2023], dots=True, id='my-range-slider'), 
-    html.Div(id='crossfilter-year--slider')
+                dcc.Dropdown(id="loc-dropdown", options=[{'label': 'Søtorvet 5, København N', 'value': 2}, {'label': 'Krügersgade 5, København K', 'value': 1}, {
+                             'label': 'Folehaven 72, Valby', 'value': 3}, {'label': 'Hillerødgade 79, København N', 'value': 4}, {'label': 'Backersvej/Formosavej, København S', 'value': 5}], value=2,  style={'font-family': 'Inter'}),
+                html.Div(id='crossfilter-year--slider', className="drop")
             ])
         ],  xs=12, lg=5, xl=10)
-    ], justify="left"),
-
-    dbc.Row([
-        dbc.Col([
-                html.Div(
-                children="", className="box1",
-                style={
-                    'backgroundColor': 'rgb(54, 13, 18)',
-                    'color': 'lightsteelblue',
-                    'height': '30px',
-                    'width': '50px',
-                    'display': 'inline-block',
-                    "margin-left": '100px',
-                    "margin-top": '40px'
-
-                }
-
-
-            ), html.Div(children="Overskrider WHO grænseværdi", style={'textAlign': 'center', 'display': 'inline-block', "margin-left": '15px'}),
-                            html.Div(
-                children="", className="box1",
-                style={
-                    'backgroundColor': 'rgb(240, 236, 236)',
-                    'color': 'lightsteelblue',
-                    'height': '30px',
-                    'width': '50px',
-                    'display': 'inline-block',
-                    "margin-left": '100px',
-                    "margin-top": '40px'
-
-                }
-
-
-            ), html.Div(children="Overskrider ikke WHO grænseværdi", style={'textAlign': 'center', 'display': 'inline-block', "margin-left": '15px'}),
-
-    
-            
-        ],xs=4, lg=5, xl=10)
-    ], justify="left"),
+    ], justify="center"),
 
 
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot')
-        ], xs=12, lg=5, xl=10),
+            html.Div([
+                dcc.Graph(id='plot')
+            ], style={"margin-top": "50px"}, className='firstplot')
+        ], xs=12, lg=5, xl=12),
 
-    ]),
+    ], justify="center"),
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id='plot2')
- 
-        ], xs=12, lg=5, xl=10)
+            html.Div([
+                dcc.Graph(id='plot2')
+            ], style={"margin-top": "50px"}, className='firstplot')
+
+        ], xs=12, lg=5, xl=12)
     ], justify="left"),
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id="plot3")
-        ], xs=12, lg=5, xl=10)
+            html.Div([
+                dcc.Graph(id='plot3')
+            ], style={"margin-top": "50px", "margin-bottom": "40px"}, className='firstplot')
+        ], xs=12, lg=5, xl=12)
     ])
 
 
-])
+], class_name='firstplot1')
 
 
-def get_range_val(range, list):
+def make_graph(df, title):
+    f = px.imshow(df, template="ggplot2", color_continuous_scale="turbid",  # amp
+                  labels=dict(color="Antal overskridelser"),
+                  x=['Jan', 'Feb', 'Mar', 'Apr', 'Maj', "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"], text_auto=True, range_color=[0, 16]
 
-    for year in range:
-        if year not in list:
-            list.append(year)
-            list.sort()
-    print(range)
-    return list
+                  )
+
+    f.update_layout(
+        yaxis_nticks=5,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgb(245,244,244)',
+        width=1250,
+        height=450,
+
+
+        title=dict(text=f'<b>{title}<b>', y=0.98, x=0, xanchor='left',
+                   yanchor='top', font=dict(family='Inter Black 900', size=20)),
+
+
+
+        legend_title="Legend Title",
+        font=dict(
+            family="Inter",
+            size=17,
+            color="rgb(126,121,120)"
+        ),
+        yaxis_title=None,
+        xaxis_title=None
+    )
+
+    f.update_xaxes(side="top")
+    f.update_traces(hoverongaps=False)
+    f.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
+
+    return f
+
 
 @app.callback(
     Output('plot', 'figure'),
-    Output('plot2','figure'),
-    Output('plot3','figure'),
-    Input('my-range-slider', 'value'))
-
-
-
+    Output('plot2', 'figure'),
+    Output('plot3', 'figure'),
+    Output('heading', component_property='children'),
+    Input('loc-dropdown', 'value'))
 def update_graph(val):
-    print(val)
-    # instead of updating graph from three seperate calls, make all the calls from here
-    vars = ["ExceededPM2","ExceededPM10","ExceededNO2"]
-    if len(set(val)) == 1:
-        df_sorted = df.loc[df['year']==val[0]]
-    else:
-        year_range = range(val[0],val[1])
-        range_val = get_range_val(year_range, val)
-        df_sorted = df.loc[df['year'].isin(range_val)]
+    dict = {1: "Krügersgade 5", 2: "Søtorvet 5", 3: "Folehaven 72",
+            4: "Hillerødgade 79", 5: "Backersvej/Formosavej"}
 
-    
-    def get_count(df):
-        return (df == 1).sum()
+    name = dict[val]
+    df = get_data(val)
+    df = process_data(df)
+    pollution = df.pivot_table(
+        values='ExceededPM2', columns="month", index="year", aggfunc=np.sum)
+    pollution2 = df.pivot_table(
+        values='ExceededPM10', columns="month", index="year", aggfunc=np.sum)
+    pollution3 = df.pivot_table(
+        values='ExceededNO2', columns="month", index="year", aggfunc=np.sum)
 
-    #df_sorted = df.loc[df['year'].isin(range_val)]
+    fig = make_graph(pollution, "PM2.5")
+    fig2 = make_graph(pollution2, "PM10")
+    fig3 = make_graph(pollution3, "NO2")
 
-    df_piv = pivot_data(df_sorted, "ExceededPM2")
-    #print((df_sorted["ExceededPM2"]==1).sum())
-    df_piv2 = pivot_data(df_sorted, "ExceededPM10")
-    df_piv3 = pivot_data(df_sorted, "ExceededNO2")
+    return fig, fig2, fig3, name
 
-    df_list = [df_piv,df_piv2, df_piv3]
-
-    names = {"ExceededPM2": "PM2.5",
-            "ExceededPM10": "PM10", "ExceededNO2": "NO2"}
-    exceeded = {0: "Nej", 1: "Ja"}
-    fig = px.imshow(df_piv, template="ggplot2", 
-                    title=f'PM2.5 målt ved søtorvet 5. <br> Antal overskridelser af grænseværdi: {(df_sorted["ExceededPM2"]==1).sum()} dage', zmin=0, zmax=1, color_continuous_scale = 'amp'  ) #[0, 'rgb(240, 236, 236)'], [1, 'rgb(215,192,184)']]
-
-    fig.update_traces(hoverongaps=False, showscale=False,
-                    hovertemplate="Pollutant measured: %{y}"
-                    "<br>Date: %{x}"
-                    "<br>Overskrider grænseværdi: %{z}")
-    fig.update_layout(xaxis_nticks=12, xaxis_title=None)
-    fig.update_yaxes(visible=False, showticklabels=False)
-    fig.update_coloraxes(showscale=False)
-    fig.layout.coloraxis.colorbar.title = 'Title<br>Here'
-    fig.update_traces(showscale=False)
-
-
-    fig2 = px.imshow(df_piv2, template="ggplot2",
-            title=f'PM10 målt ved søtorvet 5.  <br> Antal overskridelser af grænseværdi: {(df_sorted["ExceededPM10"]==1).sum()}', zmin=0, zmax=1, color_continuous_scale='amp')
-
-    fig2.update_traces(hoverongaps=False, showscale=False,
-                    hovertemplate="Pollutant measured: %{y}"
-                    "<br>Date: %{x}"
-                    "<br>Overskrider grænseværdi: %{z}")
-    fig2.update_layout(xaxis_nticks=12, xaxis_title=None)
-    fig2.update_yaxes(visible=False, showticklabels=False)
-    fig2.update_coloraxes(showscale=False)
-    fig2.layout.coloraxis.colorbar.title = 'Title<br>Here'
-    fig2.update_traces(showscale=False)
-
-    fig3 = px.imshow(df_piv3, template="ggplot2",
-        title=f'NO2 målt ved søtorvet 5 <br> Antal overskridelser af grænseværdi: {(df_sorted["ExceededNO2"]==1).sum()}', zmin=0, zmax=1, color_continuous_scale='amp')
-
-    fig3.update_traces(hoverongaps=False, showscale=False,
-                    hovertemplate="Pollutant measured: %{y}"
-                    "<br>Date: %{x}"
-                    "<br>Overskrider grænseværdi: %{z}")
-    fig3.update_layout(xaxis_nticks=12, xaxis_title=None)
-    fig3.update_yaxes(visible=False, showticklabels=False)
-    fig3.update_coloraxes(showscale=False)
-    fig3.layout.coloraxis.colorbar.title = 'Title<br>Here'
-    fig3.update_traces(showscale=False)
-
-
-
-    
-
-    return fig, fig2, fig3
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
